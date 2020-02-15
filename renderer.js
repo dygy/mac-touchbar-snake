b7a8e40695054986e6b47969b1c61cb3354605f2
Dygy <Yukimurachris@gmail.com>
2020-02-15 18:28:23 +0300

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,24 @@
+const electron = require("electron");
+let myNotification = new Notification('Title', {
+    body: 'Lorem Ipsum Dolor Sit Amet'
+})
+function createBrowserWindow() {
+    const remote = require('electron').remote;
+    const BrowserWindow = remote.BrowserWindow;
+    const win = new BrowserWindow({
+        width: (window.innerWidth*0.8), height: (window.innerHeight*0.8),
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('index.html');
+}
+myNotification.onclick = () => {
+    console.log('Notification clicked')
+}
+const button = document.getElementById('sub');
+
+button.addEventListener('click', () => {
+    createBrowserWindow();
+});
+
